d4719fa6fafc9ded88017581a91385f4a030e49c
Logan <logan.respess@gmail.com>
2024-03-04 15:13:12 -0600

    Adding React markdown. Will be adding files to mongo for testing soon

--- backend/Controllers/BlogController.js
+++ backend/Controllers/BlogController.js
@@ -3,5 +3,6 @@ const Blog = require('../Model/Blog');
 
 //Getting the blog post and its data
 const getBlogPost = async (res, req) => {
-    
+    const foundBlog = await Blog.findOne({ id: res}).exec();
+    console.log(foundBlog);
 }
